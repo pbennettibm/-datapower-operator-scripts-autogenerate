eb6c870c95f2c1444cd84b51b723a689498e38af
pbennettibm <Patrick.Bennett@ibm.com>
2022-09-27 13:04:15 -0500

    revert script

--- migrate-backup-route.sh
+++ migrate-backup-route.sh
@@ -4,17 +4,6 @@
 NAME=$1
 PORT=$2
 
-echo $PORT
-TLSBOOL=$(bash -c 'read -e -p "Do you want TLS enabled for the port above? (yes/no): " tmp; echo $tmp')
-
-TLSCHECK=$(
-  if [ "$TLSBOOL" == "yes" ]; then
-    echo "  tls:"
-    echo "    termination: passthrough"
-    echo "  wildcardPolicy: None"
-  fi
-)
-
 #define the template.
 cat  << EOF
 kind: Route
@@ -31,5 +20,7 @@ spec:
     weight: 100
   port:
     targetPort: $NAME-$PORT
-$TLSCHECK
+  # tls:
+  #  termination: passthrough
+  # wildcardPolicy: None
 EOF
