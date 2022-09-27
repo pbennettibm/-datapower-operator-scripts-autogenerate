6146bf654ef39325d2b3031e811cafb944410726
pbennettibm <Patrick.Bennett@ibm.com>
2022-09-27 12:44:03 -0500

    remove prompts

--- migrate-backup-route.sh
+++ migrate-backup-route.sh
@@ -4,19 +4,6 @@
 NAME=$1
 PORT=$2
 
-#Bash
-read -e -p "Do you need TLS enabled for Port ${Port}? (yes/no)" TLSBOOL
-#Zsh
-read -q "TLSBOOL?Do you need TLS enabled for Port ${Port} (yes/no)?"
-
-TLSCHECK=$(
-  if [ "$TLSBOOL" == "yes" ]; then
-    echo "  tls:"
-    echo "    termination: passthrough"
-    echo "  wildcardPolicy: None"
-  fi
-)
-
 #define the template.
 cat  << EOF
 kind: Route
@@ -33,5 +20,7 @@ spec:
     weight: 100
   port:
     targetPort: $NAME-$PORT
-$TLSCHECK
+  # tls:
+  #  termination: passthrough
+  # wildcardPolicy: None
 EOF
